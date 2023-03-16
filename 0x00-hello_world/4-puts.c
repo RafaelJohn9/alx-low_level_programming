49e948ff54f5a4d5cd488c8b33083b206606bda4
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-16 11:36:44 -0700

    hello;

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
+/*simple puts program*/
 {
-	puts("\"Programming is like building a multilingual puzzle\n");	
-	return  0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
