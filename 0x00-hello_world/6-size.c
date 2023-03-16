55666c11fc1a8d24388f7d277db4102fdff9d89f
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-16 12:29:37 -0700

    all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+/**
+ * main -size is not a grandeur
+ *
+ * Return Always 0
+ */
+int main(void)
+{
+	printf("size of a char:%d",sizeof=char);
+	printf("size of a int:%d",sizeof=int);
+	printf("size of a long int:%d",sizeof=long int);
+	printf("size of a long long int:%d",sizeof=long long int);
+	printf("size of a float:%d",sizeof=float);
+        return (0);
+}	
