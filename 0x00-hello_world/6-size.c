3b58cb4143f1c375fee0587abd0a177d7c9e314e
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-16 12:40:02 -0700

    hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main -size is not a grandeur
+ * main-size is not a grandeur
  *
- * Return Always 0
+ * Return:Always 0
  */
 int main(void)
 {
-	printf("size of a char:%d",sizeof=char);
-	printf("size of a int:%d",sizeof=int);
-	printf("size of a long int:%d",sizeof=long int);
-	printf("size of a long long int:%d",sizeof=long long int);
-	printf("size of a float:%d",sizeof=float);
-        return (0);
-}	
+	printf("size of a char:%d", sizeof = char);
+	printf("size of a int:%d", sizeof = int);
+	printf("size of a long int:%d", sizeof = long int);
+	printf("size of a long long int:%d", sizeof = long long int);
+	printf("size of a float:%d", sizeof = float);
+	return (0);
+}
