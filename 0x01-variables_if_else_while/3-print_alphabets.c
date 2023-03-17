6acbeee7af0cba4103cf2dc1eda1ad4bb002fabe
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-17 12:40:26 -0700

    alphBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-alphaBET
+ * Return:0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	return (0);
+}
+
