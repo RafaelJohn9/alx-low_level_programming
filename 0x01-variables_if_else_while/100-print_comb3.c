949ec09d8ff54ee18ee4ef1938e03a941e609bdd
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-18 13:22:59 +0300

    two number combinations with commas

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,9 @@ int main(void)
 	int i;
 	int d;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (d = 0; d < 10; d++)
+		for (d = 1; d < 10; d++)
 		{
 			putchar(i + '0');
 			putchar(d + '0');
