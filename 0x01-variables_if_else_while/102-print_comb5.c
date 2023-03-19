3e7d4f0bde7ad5b6fdacf8042ba8eefe556eae6b
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-19 20:39:42 +0300

    7*

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,13 +3,13 @@
  * main-2 -two numbered print out
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	int num1,num2;
+	int num1, num2;
 
-	for (num1=0; num1 < 100; num1++)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-		for(num2=num1 + 1; num2 < 100; num2++)
+		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
