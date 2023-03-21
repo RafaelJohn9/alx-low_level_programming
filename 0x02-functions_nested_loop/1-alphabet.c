d3cb1db1cc2aa5a0b70a41221187daabaddfcfaa
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-21 08:13:59 +0300

    all lower case

---
+++ 0x02-functions_nested_loop/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_alphabet-prints all alphabet in lower case
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
+
+/**
+ * main-check the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
