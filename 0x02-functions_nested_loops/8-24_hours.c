6ca20388d2cb44021f36c458565b464b64226cc8
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-22 09:27:52 +0300

    clocking

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,19 @@
 #include "main.h"
-
 /**
  * jack_bauer-hours or minutes
  */
 void jack_bauer(void)
 {
-	int hours,hour_1, hour_2,  minutes, minute_1, minute_2;
-	
-	hour_1 = hours / 10;
-	hour_2 = hours % 10;
-
-	minute_1 = minutes / 10;
-	minute_2 = minutes % 10;
+	int hours, hour_1, hour_2,  minutes, minute_1, minute_2;
 
 	for (hours = 0; hours <= 23; hours++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
 			{
+				hour_1 = hours / 10;
+				hour_2 = hours % 10;
+				minute_1 = minutes / 10;
+				minute_2 = minutes % 10;
 				if (hours < 10 && minutes < 10)
 				{
 					_putchar('0');
@@ -26,20 +23,14 @@ void jack_bauer(void)
 					_putchar(minutes + '0');
 					_putchar('\n');
 				}
-				else if (hours < 10)
+				else if (hours < 10 || minutes < 10)
 				{
 					_putchar('0');
-					_putchar(hours + '0');
+					_putchar(hour_1 + '0');
+					_putchar(hour_2 + '0');
 					_putchar(':');
-					_putchar(minutes + '0');
-					_putchar('\n');
-				}
-				else if (minutes < 10)
-				{
-					_putchar(hours + '0');
-					_putchar(':');
-					_putchar('0');
-					_putchar(minutes + '0');
+					_putchar(minute_1 + '0');
+					_putchar(minute_2 + '0');
 					_putchar('\n');
 				}
 				else
