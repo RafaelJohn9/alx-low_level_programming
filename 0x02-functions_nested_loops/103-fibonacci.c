d997b34f1ceab0d2b323b77b32292531fc97cd4a
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-22 16:11:09 +0300

    sum of the fibbonaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main-addition of even fibbonaci
+ * Return:always 0
+ */
+int main(void)
+{
+	long int limiter = 0;
+	long int a = 1;
+	long int b = 2;
+	long int next = a + b;
+	long int c;
+	long int sum = b;
+
+	while (b < 4000000)
+	{
+		c = next;
+		next = c + b;
+                b = c;
+	}
+	if (next % 2 == 0);
+	{
+		sum = next + sum;
+		printf("%ld\n",sum);
+	}
+	return (0);
+}
+
