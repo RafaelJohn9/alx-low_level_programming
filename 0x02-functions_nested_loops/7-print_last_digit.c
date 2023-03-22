6cd5fa487f0dfd9bc41e6b7c3006eeea650f305e
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-22 19:58:35 +0300

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,12 @@ int print_last_digit(int c)
 {
 	int last_digit;
 
-	if (c < 0)
+	last_digit = c % 10;
+
+	if (last_digit < 0)
 	{
 		c = -c;
 	}
-	last_digit = c % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
