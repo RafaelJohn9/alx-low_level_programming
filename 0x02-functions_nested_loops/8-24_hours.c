3b9070a59885f0f1a7cea4cef8647e7151f8b315
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-22 09:35:54 +0300

    clockin

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,38 +9,38 @@ void jack_bauer(void)
 	for (hours = 0; hours <= 23; hours++)
 	{
 		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			hour_1 = hours / 10;
+			hour_2 = hours % 10;
+			minute_1 = minutes / 10;
+			minute_2 = minutes % 10;
+			if (hours < 10 && minutes < 10)
 			{
-				hour_1 = hours / 10;
-				hour_2 = hours % 10;
-				minute_1 = minutes / 10;
-				minute_2 = minutes % 10;
-				if (hours < 10 && minutes < 10)
-				{
-					_putchar('0' + hour_1);
-					_putchar(hour_2 + '0');
-					_putchar(':');
-					_putchar('0'+ minute_1);
-					_putchar(minute_2 + '0');
-					_putchar('\n');
-				}
-				else if (hours < 10 || minutes < 10)
-				{
-					_putchar(hour_1 + '0');
-					_putchar(hour_2 + '0');
-					_putchar(':');
-					_putchar(minute_1 + '0');
-					_putchar(minute_2 + '0');
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(hour_1 + '0');
-					_putchar(hour_2 + '0');
-					_putchar(':');
-					_putchar(minute_1 + '0');
-					_putchar(minute_2 + '0');
-					_putchar('\n');
-				}
+				_putchar('0' + hour_1);
+				_putchar(hour_2 + '0');
+				_putchar(':');
+				_putchar('0' + minute_1);
+				_putchar(minute_2 + '0');
+				_putchar('\n');
 			}
+			else if (hours < 10 || minutes < 10)
+			{
+				_putchar(hour_1 + '0');
+				_putchar(hour_2 + '0');
+				_putchar(':');
+				_putchar(minute_1 + '0');
+				_putchar(minute_2 + '0');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(hour_1 + '0');
+				_putchar(hour_2 + '0');
+				_putchar(':');
+				_putchar(minute_1 + '0');
+				_putchar(minute_2 + '0');
+				_putchar('\n');
+			}
+		}
 	}
 }
