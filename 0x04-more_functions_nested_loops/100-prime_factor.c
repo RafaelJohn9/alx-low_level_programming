65ab3818cd8f3696a800b0ee9c5d255b71f53142
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-24 19:38:22 +0300

    task 12 -prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-biggest prime number
+ * Return:always 0
+ */
+int main(void)
+{
+	long long int number = 612852475143;
+	int a, prime;
+
+	for (a = 2;a <= number; a++)
+	{
+		while (a % number == 0)
+		{
+			prime = a;
+			number = number / a;
+		}
+	}
+	printf("%d", prime);
+	return (0);
+}
