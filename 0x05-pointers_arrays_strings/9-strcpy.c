ec38d7584b1ae38bef414c5ccfb16d72fd62bec7
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-03-27 21:28:59 +0300

    9 strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len = strlen(src);
 	int limiter;
 
-	for (limiter = 0; limiter <= len; limiter++)
+	while (limiter != '\0')
 	{
 		dest[limiter] = src[limiter];
+		limiter++
 	}
 	return (dest);
 }
