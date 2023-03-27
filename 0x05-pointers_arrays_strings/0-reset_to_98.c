a8a0d6f9beb70a8d041cd8cc6c83fa29ceb3d6dd
RafaelJohn9 <rafaeljohb@gmail.com>
2023-03-27 10:00:19 +0300

    resets number to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98-rewrites any number to 98
+ * @*n:value to be manipulated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+this repository we will be working with pointers,array and strings
+
+They are 11 projects in total. Their names give brief descrptions to what they are about
