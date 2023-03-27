f9413ffb7390c22df537c92b58919fd5ae659414
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-03-27 20:52:25 +0300

    array of limit ...

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include  <stdio.h>
+
+/**
+ * print_array-gives an array of n values
+ * @a:array
+ * @n:limiter
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b <= n; b++)
+	{
+		_putchar(a[b]);
+		_putchar(',');
+		_putchar(' ');
+	}
+}
