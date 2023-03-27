7659473fee2e2991912c8217c95bb998a71e9674
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-03-27 20:32:21 +0300

    half input

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,33 +8,26 @@
 void puts_half(char *str)
 {
 	int len = strlen(str);
+	int len_2 = len;
+	int half_len;
 
-	int half_len = len / 2;
-	int n = ++half_len
+	if (len % 2 == 0)
+	{
+		half_len = len / 2;
+	}
+	else
+	{
+		half_len = (len / 2) + 1;
+	}
+
+	int n = half_len + 1;
 
 	while (len >= 0)
 	{
-
-		if (len > half_len)
+		for (n; n < len_2; n++)
 		{
-			if (len % 2 == 0)
-			{
-				_putchar(str[half_len]);
-			}
-			else
-			{
-				_putchar(str[n]);
-			}
-				
-		}
-		else
-		{
-			continue;
+			_putchar(str[n]);
 		}
 		len--;
-		half_len++;
-		n++;
-
 	}
-	_putchar('\n');
 }
