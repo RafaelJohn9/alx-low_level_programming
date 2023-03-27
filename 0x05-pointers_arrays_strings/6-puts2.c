32c12dadf3378b58cb757ea2531a9640679a1137
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-03-27 18:30:52 +0300

    only half printed

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2-prints alternate numbers
+ * @str:value to be manipulated
+ */
+void puts2(char *str)
+{
+	int limiter = 0;
+
+	while (str[limiter] != '\0')
+	{
+		if (limiter % 2 == 0 || limiter == 0)
+		{
+			_putchar(s[len]);
+			limiter++;
+		}
+	}
+	_putchar('\n');
+}
