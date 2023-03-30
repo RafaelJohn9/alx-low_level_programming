9bbee966d2c4325df966842f65e74e877a8535f4
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-03-30 18:00:21 +0300

    mandatory

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *str)
 	for (a = 0; a < len; a++)
 	{
 		if (str[a] == '\n' || str[a] == '!' ||
-				str[a] == ' ' || str[a] == '.' || str[a] == '\t')
+				str[a] == ' ' || str[a] == '.' || str[a] == '\t' || str[a] == ',')
 		{
 			if (str[a + i] >= 97 && str[a + i] <= 122)
 			{
