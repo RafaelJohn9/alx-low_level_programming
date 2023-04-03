14c2bb41167afd57576a6c668f33076ff3cc9fc3
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-03 23:19:00 +0300

    sum of diadonals

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 	int n;
 	int b;
 
-	for (n = 0; n < 8 '\0'; n++)
+	for (n = 0; n < 8; n++)
 	{
 		for (b = 0; b < 8; b++)
 		{
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums-adds and prints out sum of diagonals of matrices
+ * @a:array
+ * @size:array size
+ */
+void print_diagsums(int *a, int size)
+{
+	int row;
+	int column;
+	int sum_1 = 0;
+	int sum_2 = 0;
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+		{
+			if (row == column)
+			{
+				sum_1 += (row * size + column);
+			}
+			else if (row + column == size - 1)
+			{
+				sum_2 += (row * size + column);
+			}
+		}
+	}
+	printf("%d, %d\n", sum_1, sum_2);
+}
