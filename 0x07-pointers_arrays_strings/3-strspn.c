df253b9b88a3a42a9a2ee20ed9d7bc5967954caa
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-03 21:27:38 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strspn-length of a prefix of a substring
+ * @s:value 1
+ * @accept:value 2
+ * Return:number of times
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a;
+	int b;
+	unsigned int num = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				num++;
+			}
+			else if (s[a] == ' ' || s[a] == ',')
+			{
+				break;
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+	return (num);
+}
