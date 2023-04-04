0c5410e7960c6da20380e3609b225e877900c1f6
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-04 21:09:45 +0300

    haystack

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,4 +8,25 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int a = 0;
+	int b = 0;
+	int len = 0;
+
+	while (haystack[a] != '\0')
+	{
+		while (needle[b] == haystack[a])
+		{
+			b++;
+		}
+		a++;
+	}
+	while (needle[len] != '\0')
+	{
+		len++;
+	}
+	if ( b == (len -1))
+	{
+		return (needle);
+	}
+	return ('\0');
 }
