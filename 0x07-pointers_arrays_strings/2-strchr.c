d3edc61038ccabdb7c784e9ff8e482bdf905ceda
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-04 19:41:46 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	if (c == '\0')
+	{
+		return ('\0');
+	}
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (s + i);
 		}
-	}
-	if (c == '\0')
-	{
-		return ('\0');
 	}
 	return (NULL);
 }
