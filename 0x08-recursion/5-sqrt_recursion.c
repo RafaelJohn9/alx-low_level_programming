41adbc1009e3d5d35328ffcd447de4f69a532f20
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-05 20:48:15 +0300

    task2,3,4

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * @_sqrt_recursion-returns the natural square root of a number
+ * @n:value 1
+ * Return:prints answer
+ */
+int _sqrt_recursion(int n)
+{
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * is_prime_number-prints prime number
+ * @n:value 
+ * Return:answer
+ */
+int is_prime_number(int n)
+{
