f6d541aeffea2121fb6a0b9a3403fa393c530107
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-05 20:49:12 +0300

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,10 +14,11 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 0)
 	{
-		return (1)
+		return (1);
 	}
 	else
 	{
-		return(x * _pow_recursion(x * x, y - 1))
+		return(x * _pow_recursion(x * x, y - 1));
+	}
 		
 }
