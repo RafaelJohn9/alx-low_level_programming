6a610fd0b1940a61eecd27ed960c11b54a3684ab
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-06 11:57:27 +0300

    all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * @_sqrt_recursion-returns the natural square root of a number
+ * _sqrt_recursion-returns the natural square root of a number
  * @n:value 1
  * Return:prints answer
  */
