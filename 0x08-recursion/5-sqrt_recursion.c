d6211aba43a0132c3b05c97afad5ca604cb2c5ff
RafaelJohn9 <Rafaeljohb@gmail.com>
2023-04-06 11:55:13 +0300

    all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,14 +21,14 @@ int _sqrt_recursion(int n)
 	}
 	else if (n % 49 == 0)
 	{
-		return(7 * _sqrt_recursion(n / 49));
+		return (7 * _sqrt_recursion(n / 49));
 	}
 	else if (n == 1)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
-		return(-1);
+		return (-1);
 	}
 }
