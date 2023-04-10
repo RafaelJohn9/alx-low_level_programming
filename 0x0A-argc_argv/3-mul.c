def249111d7a684be4dcc8d49b7bd0928f3fd847
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-10 09:29:34 +0300

    prints result of multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,16 @@
  */
 int main(int argc, char **argv)
 {
-	int num_1 = atoi(argv[1]);
-	int num_2 = atoi(argv[2]);
+	int num_1;
+	int num_2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num_1 = atoi(argv[1]);
+	num_2 = atoi(argv[2]);
 	printf("%d\n", num_1 * num_2);
 	return (0);
 }
