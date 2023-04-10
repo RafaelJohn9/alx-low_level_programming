4ea2b480585e5751c64e06ca4be18ab64c0eca87
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-10 11:46:40 +0300

    addition

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-program that prints the min num pf coins to mak chang for an amt of money
+ * @argc:num of arg
+ * @argv:arg 
+ * Return:always 0
+ */
+int main(int argc, char **argv)
+{
+	:wq
+
