f78d36e9606f0c5834548e847e80c8f3cc76aca8
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-11 19:59:37 +0300

    c malloc free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -5,12 +5,22 @@
  * alloc_grid-returns a ptr to a 2d array of integers
  * @width:array width
  * @height:array height
+ * Return:array of int
  */
 int **alloc_grid(int width, int height)
 {
-	int **mem ;
+	int **mem;
 	int i, j;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	mem = malloc(sizeof(width * height));
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
