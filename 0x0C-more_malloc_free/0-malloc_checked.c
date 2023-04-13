5ee530cbb35fea87070b1b5e41164378c1b45517
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-13 14:51:47 +0300

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked-function that allocates memory using malloc
+ * @b:memory allocated
+ * Return:void type
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory is ablout mallocs again, but it focuses on nested on blocks og malloc
