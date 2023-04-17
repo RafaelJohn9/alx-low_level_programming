6212cfa5f3029a5e40fc5553eed7047d949608c8
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-17 18:34:09 +0300

    4-newdog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,6 +6,7 @@
  * @name:name of dog
  * @age:age of dog
  * @owner:owner of dog
+ * Return:new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -16,9 +17,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
+	if ((*new_dog).name == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_dog->name = name;
+	}
 	new_dog->age = age;
-	new_dog->owner = owner;
+	if ((*new_dog).owner == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		new_dog->owner = owner;
+	}
 	return (new_dog);
 
 }
