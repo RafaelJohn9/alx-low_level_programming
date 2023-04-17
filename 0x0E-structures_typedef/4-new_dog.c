dd585813f585fd07a72cc081390396d2919fb61c
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-17 16:48:58 +0300

    hello

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * new_dog- new dog
+ * @name:name of dog
+ * @age:age of dog
+ * @owner:owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	name = name;
+	age = age;
+	owner = owner;
+	return (&new_dog);
+}
