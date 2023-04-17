74e9e903aa4fc79a8f98f3fa0128131e4ae563ef
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-17 16:59:50 +0300

    hello

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,16 +11,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 	{
 		return;
-	}	
+	}
 	if ((*d).name == NULL)
-	{		
+	{
 		printf("Name: (nil)\n");
 	}
 	else
 	{
 		printf("Name: %s\n", (*d).name);
 	}
-		
 	if ((*d).age > 0)
 	{
 		printf("Age: %.6f\n", (*d).age);
