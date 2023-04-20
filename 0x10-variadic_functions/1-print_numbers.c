2b16ae575da01e610bf4f78c0fa4c7a27e381a2e
RafaelJohn9 <rafaeljohb@gmail.com>
2023-04-20 19:04:45 +0300

    task 1 value os varable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arg;
 	int i, x;
 
-	if (separator == NULL || arg == NULL)
-	{
-		return;
-	}
 	va_start(arg, n);
 	for (i = 0; i < (int)n; i++)
 	{
@@ -24,6 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != (int)n - 1)
 		{
 			printf("%d%s", x, separator);
+		}
+		else if (separator == NULL)
+		{
+		printf("%d", x);
 		}
 		else
 		{
