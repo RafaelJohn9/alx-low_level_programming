9ced49514bf3605820c3b4efd4997ffb17d824dc
RafaelJohn9 <rafaeljohb@gmail.com>
2023-05-03 09:52:14 +0300

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_num->next = *head;
-		current = new_num;
 		*head = current;
 		return (*head);
 	}
