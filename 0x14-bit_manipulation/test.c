b30dc204e153852e884ecd6ffce2602083d18bfa
RafaelJohn9 <rafaeljohb@gmail.com>
2023-05-04 21:52:41 +0300

    task  3

--- 0x14-bit_manipulation/test.c
+++ 0x14-bit_manipulation/test.c
@@ -8,17 +8,16 @@
  *     */
 int main(void)
 {
-	int n;
-	
-	n = get_bit(1024, 10);
-	printf("%d\n", n);
-	n = get_bit(98, 1);
-	printf("%d\n", n);
-	n = get_bit(1024, 0);
-	printf("%d\n", n);
-	n = get_bit(124, 1);
-	printf("%d\n", n);
-	n = get_bit(10, 10);
-	printf("%d\n", n);
+	unsigned long int n;
+
+	n = 1024;
+	set_bit(&n, 5);
+	printf("%lu\n", n);
+	n = 0;
+	set_bit(&n, 10);
+	printf("%lu\n", n);
+	n = 98;
+	set_bit(&n, 0);
+	printf("%lu\n", n);
 	return (0);
 }
