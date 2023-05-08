d38c817cff5dd1d0ed63c6a1eeb7d9faded8a4e8
RafaelJohn9 <rafaeljohb@gmail.com>
2023-05-08 20:15:44 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,19 +14,27 @@ int main(int argc, char **argv)
 	struct stat file_stat;
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	return (97);
+		return (97);
+	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	return (98);
+		return (98);
+	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-	return (99);
+		return (99);
+	}
 	if (fstat(fd1, &file_stat) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	return (98);
+		return (98);
+	}
 	bytes = file_stat.st_size;
 	while (bytes > 0)
 	{
@@ -40,7 +48,9 @@ int main(int argc, char **argv)
 		bytes -= bytes_read;
 	}
 	if (close(fd1) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-	return (100);
+		return (100);
+	}
 	return (0);
 }
