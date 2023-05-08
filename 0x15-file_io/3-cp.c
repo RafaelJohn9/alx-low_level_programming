1ac484137fd40b5646dbc7c619f6009f56756c23
RafaelJohn9 <rafaeljohb@gmail.com>
2023-05-08 20:13:08 +0300

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-
+#include "main.h"
 #define BUFFER_SIZE 1024
 
 /**
- *  * main - copies the content of a file to another file
- *   * @argc: number of arguments
- *    * @argv: array of arguments
- *     * Return: 0 on success, other values on error
- *      */
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, other values on error
+ */
 int main(int argc, char **argv)
 {
 	int fd1, fd2, bytes, bytes_read;
@@ -19,48 +14,33 @@ int main(int argc, char **argv)
 	struct stat file_stat;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
-	}
-	
+	return (97);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+	return (98);
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		return (99);
-	}
+	return (99);
 	if (fstat(fd1, &file_stat) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+	return (98);
 	bytes = file_stat.st_size;
 	while (bytes > 0)
 	{
 		bytes_read = read(fd1, buffer, BUFFER_SIZE);
 		if (bytes_read == -1)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			return (98);
-		}
+		return (98);
 		if (write(fd2, buffer, bytes_read) != bytes_read)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			return (99);
-		}
+		return (99);
 		bytes -= bytes_read;
 	}
 	if (close(fd1) == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		return (100);
-	}
+	return (100);
 	return (0);
 }
