e9c633b98f6297d2503f615c196c00fe49614c8f
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-15 21:58:11 +0300

    solved betty task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,22 +4,20 @@
  * @h:pointer to a pointer to a node struct
  * @idx:index
  * @n:num to insert
+ * Return:null if not succesful else list
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	dlistint_t *new_node, *current_node;
+	dlistint_t *new_node, *current_node = *h;
 
 	if (!*h)
 	{
 		return (NULL);
 	}
-	current_node = *h;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
-	{
 		return (NULL);
-	}
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
@@ -32,21 +30,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				(current_node->prev)->next = new_node;
 				new_node->next = current_node;
 				(current_node->next)->prev = new_node;
-				return (*h);
 			}
 			else if (current_node->prev == NULL)
 			{
 				new_node->next = *h;
 				(*h)->prev = new_node;
 				*h = new_node;
-				return (*h);
-
 			}
 			else if (current_node->next == NULL)
 			{
 				current_node->next = new_node;
-				return (*h);
 			}
+			return (*h);
 		}
 		current_node = current_node->next;
 		count++;
