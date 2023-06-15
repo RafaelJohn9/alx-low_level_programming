659b9876ae610e6f931d4abf737284e619a43ca6
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-15 22:17:59 +0300

    inserting node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,10 +11,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int count = 0;
 	dlistint_t *new_node, *current_node = *h;
 
-	if (!*h)
-	{
+	if (*h == NULL  || h == NULL)
 		return (NULL);
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
@@ -28,20 +26,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (current_node->prev && current_node->next)
 			{
 				(current_node->prev)->next = new_node;
+				new_node->prev = curren_node->prev;
 				new_node->next = current_node;
-				(current_node->next)->prev = new_node;
+				current_node->prev = new_node;
 			}
 			else if (current_node->prev == NULL)
-			{
 				new_node->next = *h;
 				(*h)->prev = new_node;
 				*h = new_node;
-			}
 			else if (current_node->next == NULL)
 			{
-				current_node->next = new_node;
+				(current_node->prev)->next = new_node;
+				new_node->next = current_node;
+				new_node->prev = current_node->prev;
+				current_node->prev = new_node;
 			}
-			return (*h);
+			return (new_node);
 		}
 		current_node = current_node->next;
 		count++;
