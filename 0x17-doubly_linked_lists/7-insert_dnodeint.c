b7d87f95ef457c11ca64beb39672c039f43e00bf
RafaelJohn9 <rafaeljohb@gmail.com>
2023-06-16 20:12:57 +0300

    bug:corrects addition of an element in the start of a list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,10 +35,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			}
 			else if (current_node->prev == NULL)
 			{
-				new_node->next = *h;
+				new_node->next = current_node;
 				new_node->prev = current_node->prev;
-				(*h)->prev = new_node;
-				*h = new_node;
+				current_node->prev = new_node;`
 			}
 			return (new_node);
 		}
