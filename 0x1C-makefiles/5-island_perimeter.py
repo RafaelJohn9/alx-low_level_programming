88fb9aeb382c0d2604301ac5b7a52c49891f43f1
RafaelJohn9 <rafaeljohb@gmail.com>
2023-08-23 16:50:34 +0300

    the island -perimeter used the for loop instead

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
+
 """
-a funtion that returns the perimeter of the island described in the grid
+this is the island_perimeter
 """
 
 
 def island_perimeter(grid):
     """
-    gets the perimeter of the island
+    calculate the perimeter of the island
     """
 
-    numberOfColumns = len(grid[0])
     numberOfRows = len(grid)
+    numberOfColumns = len(grid[0])
     perimeter = 0
-    row = 0
-    column = 0
-    totalItems = numberOfRows * numberOfColumns
-    item = 1
 
-    while item < totalItems:
-        # check for the end of the row
-        if column == numberOfColumns:
-            row += 1
-            column = 0
+    # Iterate through the grid using nested loops
+    for row in range(numberOfRows):
+        for column in range(numberOfColumns):
+            if grid[row][column] == 1:
+                perimeter += 4  # Start by assuming all 4 sides are exposed
 
-        # searches for the island  and counts perimeter
-        if grid[row][column] == 1:
-            # for the row counting
-            if row > 0:
-                if grid[row-1][column] == 0:
-                    perimeter += 1
-            if row < (numberOfRows - 1):
-                if grid[row+1][column] == 0:
-                    perimeter += 1
-            # for the column counting
-            if column > 0:
-                if grid[row][column-1] == 0:
-                    perimeter += 1
-            if column < (numberOfColumns - 1):
-                if grid[row][column+1] == 0:
-                    perimeter += 1
-        item += 1
-        column += 1
+                # Check the neighbors (up, down, left, right)
+                if row > 0 and grid[row - 1][column] == 1:
+                    perimeter -= 1  # Subtract 1 for each shared side
+                if row < numberOfRows - 1 and grid[row + 1][column] == 1:
+                    perimeter -= 1
+                if column > 0 and grid[row][column - 1] == 1:
+                    perimeter -= 1
+                if column < numberOfColumns - 1 and grid[row][column + 1] == 1:
+                    perimeter -= 1
 
-    return (perimeter)
+    return perimeter
