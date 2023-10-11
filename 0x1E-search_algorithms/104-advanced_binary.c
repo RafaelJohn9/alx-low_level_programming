2d68120de7a1cff8b98a99ef5ae0ee12481098f5
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-11 11:46:41 +0300

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary- a function that searches for a value using advanced binary search
+ * advanced_binary- a function that searches for a value using advanced BSH
  * @array: array to be searched
  * @size: size of the array
  * @value: the value to be searched
@@ -13,33 +13,40 @@ int advanced_binary(int *array, size_t size, int value)
 	size_t right = size - 1;
 	size_t mid;
 	size_t i;
+	int result = -1;
 
 	if (!array || size == 0 || !value)
 		return (-1);
 
-	while (left <= right)
+	mid = left + (right - left) / 2;
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
 	{
-		mid = left + (right - left) / 2;
-
-		printf("Searching in array: ");
-		for (i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i != size - 1)
-				printf(", ");
-		}
-		printf("\n");
-		if (array[mid] == value && array[mid - 1] != value)
-			return (mid);
-		else if (array[mid] < value)
-			left = mid + 1;
-		else
-		{
-			if (array[mid] == value)
-				right = mid;
-			else
-				right = mid - 1;
-		}
+		printf("%d", array[i]);
+		if (i != size - 1)
+			printf(", ");
 	}
-	return (-1);
+	printf("\n");
+
+	if (array[mid] == value && array[mid - 1] != value)
+		return (mid);
+	else if (array[mid] < value)
+	{
+		left = mid + 1;
+		result = advanced_binary(array + left, size - left, value);
+
+		if (result != -1)
+			result += mid + 1;
+		return (result);
+	}
+	else
+	{
+		if (array[mid] == value)
+			right = mid;
+		else
+			right = mid - 1;
+		result = advanced_binary(array, right, value);
+		return (result);
+	}
+	return (result ? array[result] == value : -1);
 }
