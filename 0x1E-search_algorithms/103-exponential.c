cccce893fd86874e2839bdbc0d32958c65bf3cb4
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-11 09:32:39 +0300

    exponential search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -9,67 +9,43 @@
  */
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t i = 0, j = 0, high = size - 1, low = 0, mid = 0;
+	size_t low, high, mid, bound, i;
 
-	if (!array || !value || size == 0)
+	if (!array || size == 0)
 		return (-1);
-
 	if (array[0] == value)
 	{
-		printf("Value checked array[0] = [%d]", array[0]);
+		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (0);
 	}
-	for (i = 1; i < size; i *= 2)
+	bound = 1;
+	while (bound < size && array[bound] < value)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] < value)
-			continue;
-		else if (array[i] == value)
-		{
-			return (i);
-		}
-		else
-		{
-			break;
-		}
+		printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+		bound *= 2;
 	}
-
-	printf("Value found between indexes [%lu] and [%lu]\n", (i / 2), (i - 1));
-	high = i - 1;
-	low = i / 2;
-
-	while (1)
+	printf("Value found between indexes [%lu] and [%lu]\n", bound / 2, bound - 1);
+	low = bound / 2;
+	high = (bound < size - 1) ? bound : size - 1;
+	while (low <= high)
 	{
+		mid = (low + high) / 2;
 		printf("Searching in array: ");
-		for (j = low; j <= high; j++)
+		for (i = low; i <= high; i++)
 		{
-			printf("%d", array[j]);
-			if (j != high)
+			printf("%d", array[i]);
+			if (i < high)
 				printf(", ");
 		}
 		printf("\n");
-		if ((high - low + 1) <= 1)
-		{
-			if (array[high] == value)
-				return (high);
-			else
-				return (-1);
-		}
-		mid = (high - low + 1) / 2;
-		mid--;
-		mid += low;
+
 		if (array[mid] == value)
 			return (mid);
 		else if (array[mid] < value)
-		{
 			low = mid + 1;
-			continue;
-		}
 		else
-		{
 			high = mid - 1;
-			continue;
-		}
 	}
+
 	return (-1);
 }
