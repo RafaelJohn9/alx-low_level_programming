be0fb7aeb33857bcc8660be5992007dcf5677163
RafaelJohn9 <rafaeljohb@gmail.com>
2023-10-11 07:11:57 +0300

    timecomplexity of jumpsearch in singly linked list

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,8 +20,14 @@ int interpolation_search(int *array, size_t size, int value)
 	pos = low + (((double)(high - low) /
 				(array[high] - array[low])) * (value - array[low]));
 
-	for (i = pos; i < high;)
+	for (i = pos; i < high; i = pos)
 	{
+		if (i > high)
+		{
+			printf("Value checked array[%lu] is out of range", pos);
+			return (-1);
+		}
+
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
